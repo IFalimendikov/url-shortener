eb1af9d8d1d7cbae1cb75c332efbf42429654e52
IFalimendikov <golovanjot2@gmail.com>
2025-03-03 18:53:02 +0100

    fix flag names

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -9,8 +9,8 @@ import (
 func ParseFlags() config.Config{
 	config := config.Config{}
 	
-	flag.StringVar(&config.HTTPAddr, "addr", config.HTTPAddr, "HTTP server host address")
-	flag.StringVar(&config.BaseAddr, "base", config.BaseAddr, "Base HTTP address returned before short URL")
+	flag.StringVar(&config.HTTPAddr, "a", config.HTTPAddr, "HTTP server host address")
+	flag.StringVar(&config.BaseAddr, "b", config.BaseAddr, "Base HTTP address returned before short URL")
 	flag.Parse()
 
 	return config
