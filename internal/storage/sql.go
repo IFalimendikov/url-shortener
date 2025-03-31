7d771e6f71771facf4fd1bb29b5386e0ef908016
IFalimendikov <golovanjot2@gmail.com>
2025-03-31 21:18:41 +0200

    [fix] sql

--- internal/storage/sql.go
+++ internal/storage/sql.go
@@ -1,11 +1,11 @@
 package storage
 
 var CreateShortURLTable = `
-	CREATE TABLE IF NOT EXISTS urls (
-		id integer,
-		short_url text,
-		url text
-	);`
+    CREATE TABLE IF NOT EXISTS urls (
+        id SERIAL PRIMARY KEY,
+        short_url TEXT NOT NULL UNIQUE,
+        url TEXT NOT NULL
+    );`
 
 var GetURL = `
 	SELECT url 
