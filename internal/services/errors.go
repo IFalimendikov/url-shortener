b5b7f9c7b6852c97edc1d2b491aa6d77396b5700
IFalimendikov <golovanjot2@gmail.com>
2025-04-23 21:23:15 +0200

    fix

--- internal/services/errors.go
+++ internal/services/errors.go
@@ -8,5 +8,5 @@ var (
 	ErrorDuplicate  = errors.New("duplicate URL record")
 	ErrorNotFound   = errors.New("error finding URL")
 	ErrorURLDeleted = errors.New("URL was deleted")
-	ErrorURlSave = errors.New("can't save URL")
+	ErrorURLSave = errors.New("can't save URL")
 )
