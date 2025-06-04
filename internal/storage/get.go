8acc2af9b139c5c9b5f0d63a695a84db50032d09
IFalimendikov <golovanjot2@gmail.com>
2025-06-04 21:50:32 +0200

    test

--- internal/storage/get.go
+++ internal/storage/get.go
@@ -4,18 +4,13 @@ import (
 	"context"
 	"url-shortener/internal/models"
 
-	sq "github.com/Masterminds/squirrel"
+	// sq "github.com/Masterminds/squirrel"
 )
 
 func (s *Storage) Get(ctx context.Context, shortURL string) (string, error) {
 	var url models.URLRecord
-
-	row := sq.Select("url", "deleted").
-		From("urls").
-		Where(sq.Eq{"short_url": shortURL}).
-		PlaceholderFormat(sq.Dollar).
-		RunWith(s.DB).
-		QueryRowContext(ctx)
+	var query = `SELECT url, deleted FROM urls WHERE short_url = $1`
+	row := s.DB.QueryRowContext(ctx, query, shortURL)
 
 	err := row.Scan(&url.URL, &url.Deleted)
 	if err != nil {
