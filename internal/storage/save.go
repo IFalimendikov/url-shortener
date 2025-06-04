29871546a4bcf79a14d7cb6c9d057edae2db0fd8
IFalimendikov <golovanjot2@gmail.com>
2025-06-04 21:46:41 +0200

    test

--- internal/storage/save.go
+++ internal/storage/save.go
@@ -7,23 +7,18 @@ import (
 
 	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5/pgconn"
-	sq "github.com/Masterminds/squirrel"
+	// sq "github.com/Masterminds/squirrel"
 )
 
 func (s *Storage) Save(ctx context.Context, rec models.URLRecord) error {
-    _, err := sq.Insert("urls").
-        Columns("user_id", "short_url", "url").
-        Values(rec.UserID, rec.ShortURL, rec.URL).
-        RunWith(s.DB).
-        PlaceholderFormat(sq.Dollar).
-        ExecContext(ctx)
-
-    if err != nil {
-        var pgErr *pgconn.PgError
-        if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation {
-            return ErrorDuplicate
-        }
-        return ErrorURLSave
-    }
-    return nil
+	var query = `INSERT into urls (user_id, short_url, url) VALUES ($1, $2, $3)`
+	_, err := s.DB.ExecContext(ctx, query, rec.UserID, rec.ShortURL, rec.URL)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation {
+			return ErrorDuplicate
+		}
+		return ErrorURLSave
+	}
+	return nil
 }
